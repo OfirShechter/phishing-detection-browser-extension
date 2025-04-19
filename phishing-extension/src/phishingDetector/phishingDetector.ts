334768304b24f6989d2e1ed738780834f5a75683
Ofir Shechter <ofir1847@gmail.com>
2025-04-19 16:22:49 +0300

    on the way

--- phishing-extension/src/phishingDetector/phishingDetector.ts
+++ phishing-extension/src/phishingDetector/phishingDetector.ts
@@ -1,6 +1,11 @@
-import { urlTypePredict } from "./urlTypePredict";
+import { decisionTreeUrlClassifier } from "./decisionTree";
+import { extractUrlFeatures } from "./urlFeaturesExtractor";
 
 export function isPhishingSite(url: string): boolean {
     return urlTypePredict(url);
   }
   
+function urlTypePredict(url: string): boolean {
+    const urlFeatures = extractUrlFeatures(url);
+    return decisionTreeUrlClassifier.predict(urlFeatures) === 1; // 1 indicates phishing
+}
